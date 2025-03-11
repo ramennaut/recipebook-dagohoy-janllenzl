557f5c068945bc5e00f43f71cbf5184cd51dbbfd
Jan Llenzl Dagohoy <jan.dagohoy@student.ateneo.edu>
2025-03-11 19:20:17 +0800

    add: models

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse(
+            'ingredient_detail',
+            kwargs={'pk': self.pk}
+        )
+    
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse(
+            'recipe_detail',
+            kwargs={'pk': self.pk}
+        )
+
+class RecipeIngredient(models.Model):
+    quantity = models.CharField(max_length=100)
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        related_name="recipe"
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name="ingredients"
+    )
+
+    def __str__(self):
+        return f"{self.quantity} of {self.ingredient.name}"
+
+
--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,14 +1,9 @@
 from django.urls import path
-from .views import index
-from .views import recipes_list
-from .views import recipe_1
-from .views import recipe_2
+from .views import RecipeListView, RecipeDetailView
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('recipes/list/', recipes_list, name='recipes_list'),
-    path('recipe/1', recipe_1, name='recipe_1'),
-    path('recipe/2', recipe_2, name='recipe_2'),
+    path('recipes/list/', RecipeListView.as_view(), name='recipes_list'),
+    path('recipe/<int:pk>/', RecipeDetailView.as_view(), name='recipe_detail'),
 ]
 
 app_name = "ledger"
