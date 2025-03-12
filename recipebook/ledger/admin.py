e1232fd164c438df53501d8752cb211bd9a82bf6
Jan Llenzl Dagohoy <jan.dagohoy@student.ateneo.edu>
2025-03-12 16:56:32 +0800

    fix: admin.py

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from .models import Recipe, Ingredient, RecipeIngredient
 
 # Register your models here.
-admin.site.register(Recipe)
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeIngredientInline]
+
+admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Ingredient)
-admin.site.register(RecipeIngredient)
